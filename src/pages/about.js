92791b1a65f221334387cb14f6a2512a4834099a
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-18 21:14:24 +0300

    end of part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+	return (
+		<Layout pageTitle="About Me">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, esse.
+			</p>
+		</Layout>
+	);
+};
+
+export default AboutPage;
