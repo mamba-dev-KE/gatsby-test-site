211e20234c8b9968b41c5ab24301955739a6d5a0
Joseph Maramba <josephmaramba50@gmail.com>
2022-01-20 09:19:42 +0300

    part 5

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,6 @@ const IndexPage = () => {
 	return (
 		<Layout pageTitle="Home Page">
 			<p>Welcome to my Gatsby Site!</p>
-			<StaticImage
-				src="https://images.unsplash.com/photo-1642543492524-fa0e22fa25f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-				alt="blog"
-			/>
 			<StaticImage src="../images/hero.jpg" alt="hero" />
 		</Layout>
 	);
